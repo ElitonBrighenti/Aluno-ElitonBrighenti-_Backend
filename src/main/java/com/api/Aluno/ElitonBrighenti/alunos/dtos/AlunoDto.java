9e1525e7ea853d8261e73a6f5b5074e4c3ab7cc6
Eliton Brighenti <elitonbrighenti90@gmail.com>
2025-09-26 12:29:13 -0300

    finish commit

--- src/main/java/com/api/Aluno/ElitonBrighenti/alunos/dtos/AlunoDto.java
+++ src/main/java/com/api/Aluno/ElitonBrighenti/alunos/dtos/AlunoDto.java
@@ -8,12 +8,13 @@ import lombok.Data;
 public class AlunoDto {
 
     @NotBlank(message = "O nome é obrigatório")
-    @Size(min = 3, message = "O nome deve ter pelo menos 3 caracteres")
+    @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
     private String nome;
 
     @NotBlank(message = "O curso é obrigatório")
-    @Size(min = 3, message = "O curso deve ter pelo menos 3 caracteres")
+    @Size(min = 3, max = 100, message = "O curso deve ter entre 3 e 100 caracteres")
     private String curso;
 
+    @Size(max = 20, message = "O telefone não pode passar de 20 caracteres")
     private String telefone;
 }
